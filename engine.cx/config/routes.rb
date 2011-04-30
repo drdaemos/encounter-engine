eafdb9c0fe5c75c8edb52b41975526d41fa9a8fd
Daniel Vartanov <daniel.vartanov@gmail.com>
2011-04-30 17:56:13 +0600

    A simplest index page

--- engine.cx/config/routes.rb
+++ engine.cx/config/routes.rb
@@ -48,9 +48,7 @@ EngineCx::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "index#index"
 
   # See how all your routes lay out with "rake routes"
 
