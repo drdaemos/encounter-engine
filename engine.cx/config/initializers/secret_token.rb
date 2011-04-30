2a253e4179cb1314734c28185d59f8ebf568013a
Daniel Vartanov <daniel.vartanov@gmail.com>
2011-04-30 17:30:52 +0600

    Initial Rails 3 app with devise support

---
+++ engine.cx/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EngineCx::Application.config.secret_token = '6b14132c9760e1fc8e63391d60ca89aff88f82621ba5fedbec9acb65ab336c80db37c0d69a173c4d046968118951ef46fea9f6e71071f0d8220a78b8178dba72'
