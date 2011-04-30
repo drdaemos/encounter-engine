615ccdaef6946ae40eeea12ef9b029e841970187
Daniel Vartanov <daniel.vartanov@gmail.com>
2011-04-30 23:31:00 +0600

    Adopted rails_admin

--- engine.cx/db/schema.rb
+++ engine.cx/db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110430112805) do
+ActiveRecord::Schema.define(:version => 20110430172133) do
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.string   "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 5
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_histories_on_item_and_table_and_month_and_year"
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
