e9d8f13bad2046ce7371280ebdc7b4137fa55be3
Daniel Vartanov <daniel.vartanov@gmail.com>
2011-05-07 15:01:48 +0600

    Mailing step definitions

---
+++ engine.cx/features/step_definitions/mail_steps.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+def deliveries_for(email)
+  deliveries = []
+  ActionMailer::Base.deliveries.each do |delivery|
+    deliveries << delivery if delivery.to.include? email
+  end
+  deliveries  
+end
+
+Then %r{одно письмо с текстом "(.*)" должно быть выслано на ([^/\s]+)$}i do |text, email|  
+  deliveries = deliveries_for email
+  deliveries_for(email).size.should == 1
+  deliveries.last.text.should match(/#{text}/)
+end
+
+Then %r{никакие письма не должны быть высланы$}i do
+  ActionMailer::Base.should have(0).deliveries
+end
+
+Then %r{никакие письма не должны быть высланы на (.*)$}i do |email|
+  deliveries_for(email).should be_empty
+end
+
+Given %r{все отосланные к этому моменту письма прочитаны$}i do
+  ActionMailer::Base.deliveries.clear
+end
