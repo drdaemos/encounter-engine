f10fc7619a219934001a4d17abe071ff97350015
Daniel Vartanov <dan@vartanov.net>
2011-11-21 23:28:20 +0600

    Upgraded to Rails 3.1.3

--- engine.cx/config/initializers/secret_token.rb
+++ engine.cx/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-EngineCx::Application.config.secret_token = '6b14132c9760e1fc8e63391d60ca89aff88f82621ba5fedbec9acb65ab336c80db37c0d69a173c4d046968118951ef46fea9f6e71071f0d8220a78b8178dba72'
+EngineCx::Application.config.secret_token = 'bc80dbd062bb128c5479fb104abd6373554c86d27b8057405c98d5bb64d9b2dc438b8e45e05693d39da747547ac661b7d103005b665618b81434b2b5e9eec7f6'
--- engine.cx/config/initializers/session_store.rb
+++ engine.cx/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-EngineCx::Application.config.session_store :cookie_store, :key => '_engine.cx_session'
+EngineCx::Application.config.session_store :cookie_store, key: '_engine_cx_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
