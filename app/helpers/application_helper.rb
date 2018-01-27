4cc2cff7a19b6050f2d06ef26d5aa6f57757d279
Eugene Dementjev <daemos@x-cart.com>
2018-01-27 22:26:33 +0400

    admin levels navigation menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def is_visible_accessories_block
-    current_page?(index_page_path) or (controller_name === 'games' and action_name === 'show')
+    current_page?(index_page_path) || (controller_name === 'games' && action_name === 'show')
   end
 
   def is_visible_photos_block
